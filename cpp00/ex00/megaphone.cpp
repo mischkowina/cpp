92801d3f7ae0363743a53a6b9d1152028bdd455e
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-01-04 18:22:09 +0100

    ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,27 @@
 /*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/29 10:19:59 by smischni          #+#    #+#             */
-/*   Updated: 2022/11/29 10:20:00 by smischni         ###   ########.fr       */
+/*   Updated: 2023/01/04 18:12:14 by smischni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
