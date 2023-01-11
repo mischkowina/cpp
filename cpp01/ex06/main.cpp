691bc4c4fa7adc789f14cb77a7771b9b4d315d16
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-01-11 18:14:35 +0100

    ex06 done

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,17 +6,64 @@
 /*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 13:49:09 by smischni          #+#    #+#             */
-/*   Updated: 2023/01/11 14:39:23 by smischni         ###   ########.fr       */
+/*   Updated: 2023/01/11 18:10:38 by smischni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+int	getIdx(std::string level);
+
 int	main(int argc, char **argv)
 {
-	Harl	karen;
-
+	if (argc != 2)
+	{
+		std::cout << "Error: Only one argument allowed." << std::endl;
+		return (1);
+	}
 	
+	Harl		karen;
+	std::string	level(argv[1]);
+	int			idx;
+
+	idx = getIdx(level);
+	switch (idx)
+	{
+		case 0:
+		{
+			karen.complain("debug");
+			break;
+		}
+		case 1:
+		{
+			karen.complain("info");
+			break;
+		}
+		case 2:
+		{
+			karen.complain("warning");
+			break;
+		}
+		case 3:
+		{
+			karen.complain("error");
+			break;
+		}
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 	
 	return (0);
+}
+
+int	getIdx(std::string level)
+{
+	std::string	type[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for(int i = 0; i < 4; i++)
+	{
+		if (type[i] == level)
+			return (i);
+	}
+	return (-1);
 }
