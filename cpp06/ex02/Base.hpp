55b2e1635ae6ad6a31443ae57faa5f22cb9a1ec9
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-02-02 16:57:58 +0100

    ex02 Done

---
+++ cpp06/ex02/Base.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Base.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 16:29:45 by smischni          #+#    #+#             */
+/*   Updated: 2023/02/02 16:55:52 by smischni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASE_HPP
+# define BASE_HPP
+
+# define DEFAULT "\033[0m"
+# define YELLOW "\033[33m"
+# define PINK "\033[35m"
+
+class Base {
+	public:
+		virtual ~Base() {};
+};
+
+#endif
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 16:32:54 by smischni          #+#    #+#             */
+/*   Updated: 2023/02/02 16:56:05 by smischni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <ctime>
+#include <iostream>
+
+Base	*generate()
+{
+	time_t	timer;
+	Base	*p;
+
+	timer = time(NULL);
+	if (timer % 3 == 0)
+	{
+		p = new A;
+		std::cout << PINK "Base *p object of type A has been created." DEFAULT << std::endl;
+	}
+	else if (timer % 3 == 1)
+	{
+		p = new B;
+		std::cout << PINK "Base *p object of type B has been created." DEFAULT << std::endl;
+	}
+	else
+	{
+		p = new C;
+		std::cout << PINK "Base *p object of type C has been created." DEFAULT << std::endl;
+	}
+	return (p);
+}
+
+void	identify(Base *p)
+{
+	A	*a = dynamic_cast<A *>(p);
+	B	*b = dynamic_cast<B *>(p);
+	C	*c = dynamic_cast<C *>(p);
+
+	if (a)
+		std::cout << "Base *p object is of type A." << std::endl;
+	else if (b)
+		std::cout << "Base *p object is of type B." << std::endl;
+	else if (c)
+		std::cout << "Base *p object is of type C." << std::endl;
+}
+
+void	identify(Base &p)
+{
+	A	*a = dynamic_cast<A *>(&p);
+	B	*b = dynamic_cast<B *>(&p);
+	C	*c = dynamic_cast<C *>(&p);
+
+	if (a)
+		std::cout << "Base *p object is of type A." << std::endl;
+	else if (b)
+		std::cout << "Base *p object is of type B." << std::endl;
+	else if (c)
+		std::cout << "Base *p object is of type C." << std::endl;
+}
+
+int	main(void)
+{
+	std::cout << std::endl;
+	std::cout << YELLOW "***** Call generate()-function to create a random object *****" DEFAULT << std::endl;
+	Base *p = generate();
+	std::cout << YELLOW "***** Identify object type by handing over pointer to identify() *****" DEFAULT << std::endl;
+	identify(p);
+	std::cout << YELLOW "***** Identify object type by handing over reference to identify() *****" DEFAULT << std::endl;
+	identify(*p);
+	std::cout << std::endl;
+
+	delete p;
+}
