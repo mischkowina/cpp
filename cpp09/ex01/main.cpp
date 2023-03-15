5b5fe95daa59cd36d37c8455696c5628e7d00dd6
Saskia Mischnick <smischni@student.42wolfsburg.de>
2023-03-15 15:25:33 +0100

    cout instead cerr

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: smischni <smischni@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 12:32:30 by smischni          #+#    #+#             */
-/*   Updated: 2023/03/15 14:53:59 by smischni         ###   ########.fr       */
+/*   Updated: 2023/03/15 15:22:40 by smischni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
-		std::cerr << RED "ERROR: " DEFAULT << "Needs exactly one string as input parameter." << std::endl;
+		std::cout << RED "ERROR: " DEFAULT << "Needs exactly one string as input parameter." << std::endl;
 		return 1;
 	}
 
@@ -28,7 +28,7 @@ int	main(int argc, char **argv)
 	}
 	catch (std::exception &e)
 	{
-		std::cerr << RED "ERROR: " DEFAULT << e.what() << std::endl;
+		std::cout << RED "ERROR: " DEFAULT << e.what() << std::endl;
 		return 1;
 	}
 	
